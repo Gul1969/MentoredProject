3b5c015a62afc3c25505922df49b3bfb12b67c96
Gul1969 <71330969+Gul1969@users.noreply.github.com>
2020-11-06 17:43:45 +0000

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,15 @@
 
 #! /bin/bash
 
-sudo docker-compose push localhost:8082
+sudo docker tag project_service1 localhost:8082/project_service1
+sudo docker push localhost:8082/project_service1
+
+sudo docker tag project_service2 localhost:8082/project_service2
+sudo docker push localhost:8082/project_service2
+
+sudo docker tag project_service3 localhost:8082/project_service3
+sudo docker push localhost:8082/project_service3
+
+sudo docker tag project_service4 localhost:8082/project_service4
+sudo docker push localhost:8082/project_service4
+
