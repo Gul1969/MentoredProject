c86238eeef0c09c3ae05beed2034fefbfd591e37
Gul1969 <71330969+Gul1969@users.noreply.github.com>
2020-11-06 09:23:50 +0000

    Update ansible.sh

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-sudo apt install ansible
+sudo apt install ansible -y
 
 cd /home/student/MentoredProject/scripts/
-ansible-playbook -i '127.0.0.1' playbook.yaml 
+ansible-playbook playbook.yaml 
